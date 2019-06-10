1f62a8e2ea7ccf874d1b25eda596fed306d6c80f
Precious Keyz <ositachima.co@gmail.com>
2019-06-10 22:06:52 +0000

    refactored project

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -6,13 +6,15 @@ const targetPath = `./src/environments/environment.${env}.ts`;
 
 const envConfigFile = `export const environment = {
      production: ${env === 'prod'},
-     apiKey: "${process.env.APIKEY}",
-     authDomain: "${process.env.AUTHDOMAIN}",
-     databaseURL: "${process.env.DATABASEURL}",
-     projectId: "${process.env.PROJECTID}",
-     storageBucket: "",
-     messagingSenderId: "${process.env.MESSAGINGSENDERID}",
-     appId: "${process.env.APPID}"
+     firebase: {
+      apiKey: "${process.env.APIKEY}",
+      authDomain: "${process.env.AUTHDOMAIN}",
+      databaseURL: "${process.env.DATABASEURL}",
+      projectId: "${process.env.PROJECTID}",
+      storageBucket: "",
+      messagingSenderId: "${process.env.MESSAGINGSENDERID}",
+      appId: "${process.env.APPID}"
+     }
    };`;
 
 fs.writeFile(targetPath, envConfigFile, function(err) {
